f34a8100413f88cc54b3613d6ee0006537fb97d6
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-05-30 17:56:08 -0400

    Add functionality
    Added all the calculator functionality to the calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,13 +1,20 @@
 import '../App.css';
+import { useState } from 'react';
 import ButtonBox from './ButtonBox';
+import calculate from '../logic/calculate';
 
 const Calculator = () => {
-  const values = ['AC', '+/-', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  const values = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+  const [data, setData] = useState({ total: null, next: null, operation: null });
+
+  const press = (val) => {
+    setData(calculate(data, val));
+  };
   return (
     <div className="calculator">
-      <input type="text" className="screen" value="5x8=40" />
+      <input type="text" className="screen" value={data.next || data.total || '0'} />
       {values.map((val) => (
-        <ButtonBox key={val} value={val} />
+        <ButtonBox key={val} value={val} press={press} />
       ))}
     </div>
   );
